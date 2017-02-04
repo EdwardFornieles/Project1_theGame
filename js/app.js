20b8c54858f310cc42d919bc846470ca635001fd
alex espinoza <alexjespinozac@gmail.com>
2017-02-04 12:33:35 -0800

    starts from 0 on a second attemot to get the shoot function right. change. button id=btn, and kickTime to shoot

--- js/app.js
+++ js/app.js
@@ -1,69 +1,107 @@
 $( document ).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////             FIRST ATTEMPT             //////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
 //I need to set a timer that runs when a button
 // is pushed
 // that timer will be represented by the kickTime
 //
-var $time = $('#kickTime')
-var $shoot = ('#shoot')
-// $('body').on('click',($('#kickTime').animate({height:'0%'}))
-var $container = $('#timer')
-
-
-// //take turns.
-// the player hits the button to start the penalty kick
-// when the button is hit again it will determine based on height
-// if the shoot is a goal or a miss
-
-
- //
- //  var $turn = function turn(){
-if($kick === false){
-  var $kick = false
-    $('#shoot').on('click', function kicking(){
-    $('#kickTime').animate({height:'0%'}, 5000)})
-    animate.stop()
-
-    console.log('you miss the shoot!')
-
-
-} else if ($kick === true){
-
-    console.log('you win!')
-}
-    $('#shoot').on('click', function goal(){
-    $('kickTime').stop()})
-{
-    $kick = true
-}
-
-
-
-    // what to do if the value of $kick is true?????
-
-
-    // kicking.stop()})
-
-  // } else if ($kick === true) {
-    // console.log("That's a goal!")
-  // } $kick = true
-
-// } else {
-
-
-
-// }
-  //  $('#shoot').stop()
-// and an if else to run the turn function with original value of false
-
-
-// var $result = function result(($('#timer').height / $('#kickTime').height))
-
-
-
-// figure a way to stop with other click
-// set a way to reset the kick after stoped
-
-// once stop the motion, calculate a ratio between the two timers
-// use that ratio to stablish a winning or losing value
+// var $time = $('#kickTime')
+// var $shoot = ('#shoot')
+// // $('body').on('click',($('#kickTime').animate({height:'0%'}))
+// var $container = $('#timer')
 //
+//
+// // //take turns.
+// // the player hits the button to start the penalty kick
+// // when the button is hit again it will determine based on height
+// // if the shoot is a goal or a miss
+//
+//
+//  //
+//  //  var $turn = function turn(){
+// if($kick === false){
+//   var $kick = false
+//     var $kicking = $('#shoot').on('click', function kicking(){
+//     $('#kickTime').animate({height:'0%'}, 5000)})
+//     animate.stop()
+//
+//     console.log('you miss the shoot!')
+//
+//
+// } else if ($kick === true){
+//
+//     console.log('you win!')
+// }
+//     $('#shoot').on('click', function goal(){
+//     $('kickTime').stop()})
+// {
+//     $kick = true
+// }
+//
+//     // what to do if the value of $kick is true?????
+//
+//
+//     // kicking.stop()})
+//
+//   // } else if ($kick === true) {
+//     // console.log("That's a goal!")
+//   // } $kick = true
+//
+// // } else {
+//
+//
+//  }
+//   //  $('#shoot').stop()
+// // and an if else to run the turn function with original value of false
+//
+//
+// // var $result = function result(($('#timer').height / $('#kickTime').height))
+//
+//
+// figure a way to stop with other click
+// // set a way to reset the kick after stoped
+//
+// // once stop the motion, calculate a ratio between the two timers
+// // use that ratio to stablish a winning or losing value
+// //
 })
