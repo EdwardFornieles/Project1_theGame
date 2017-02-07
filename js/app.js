c09ab7d864784424d3aa0d386ce040577d767a15
alex espinoza <alexjespinozac@gmail.com>
2017-02-07 11:24:39 -0800

    set a stop function and working on a restart function

--- js/app.js
+++ js/app.js
@@ -1,92 +1,12 @@
 $(document).ready(function() {
 
-  // var $timer = $('#timer')
-  //     var $kick = $('#shoot')
-  //     var turn = 1
-  //     var player1 = {
-  //       team: 'home',
-  //       score: 0,
-  //  -    scoreBoard: $('#player1Score')
-  //  +    attemptNum: 0,
-  //  +    attemts: ['1stP1', '2ndP1', '3rdP1', '4thP1', '5thP1']
-  //  +    // scoreBoard: $('#player1Score')
-  //     }
-  //     var player2 = {
-  //       team: 'away',
-  //       score: 0,
-  //  -    scoreBoard: $('#player2Score')
-  //  +    attemptNum: 0,
-  //  +    attemts: ['1stP2', '2ndP2', '3rdP2', '4thP2', '5thP2']
-  //  +    // scoreBoard: $('#player2Score')
-  //     }
-  //
-  //     var kicked = false
-  //     var result = (($kick.height() / $timer.height()) * 100)
-  //
-  //
-  //  -  ////****    FUNCTIONS     *****////
-  //  +
-  //  +   ////////////////////////////////////////
-  //  +  ////****       FUNCTIONS       *****////
-  //  + ////////////////////////////////////////
-  //  +
-  //     function whosTurn() {
-  //       return (turn % 2 ? player2 : player1)
-  //     }
-  //  @@ -80,9 +43,9 @@ $(document).ready(function() {
-  //       $('#btn').on('click', function() {
-  //         ($kick).animate({
-  //           height: '100%'
-  //
-  //  +      }, 1000, function() {
-  //           alert(currentPlayer.score + " Time is up, you missed your shoot" + result)
-  //
-  //  +        setTimeout(newShoot, 60);
-  //           turn++
-  //
-  //         });
-  //  @@ -91,19 +54,29 @@ $(document).ready(function() {
-  //
-  //     $('#btn2').click(function() {
-  //       ($kick).stop()
-  //
-  //  +    var result = Math.round(($kick.height() / $timer.height()) * 100)
-  //
-  //       if (result < 95) {
-  //  +      alert(currentPlayer.score + " you missed it!  " + result)
-  //  +      setTimeout(newShoot, 60);
-  //         turn++
-  //
-  //       } else {
-  //         currentPlayer.score += 1
-  //         turn++
-  //         // console.log(whosTurn())
-  //
-  //  +      alert(currentPlayer.score + " It's a goal!  ")
-  //  +      setTimeout(newShoot(), 600)
-  //  +      goal ()
-  //  +      function goal(){
-  //  +        for(var i = 0; i<currentPlayer.attemts; i++)
-  //  +        if(currentPlayer.score !== 0){
-  //  +          currentPlayer.addClass('goal')
-  //  +        }
-  //  +        else {
-  //  +          currentPlayer.addClass('missed')
-  //  +        }
-  //  +      }
-  //       }
-  //     })
-
-
-
-   ////////////////////////////////////////
+  ////////////////////////////////////////
   ////****    GLOBAL VARIABLES    ****////
- ////////////////////////////////////////
-
+  ////////////////////////////////////////
 
   var $timer = $('#timer')
   var $kick = $('#shot')
-  var turn = 1
+  var turn = 0
   var player1 = {
     team: 'home',
     score: 0,
@@ -104,11 +24,9 @@ $(document).ready(function() {
   var kicked = false
   var result = (($kick.height() / $timer.height()) * 100)
 
-
-   ////////////////////////////////////////
+  ////////////////////////////////////////
   ////****       FUNCTIONS       *****////
- ////////////////////////////////////////
-
+  ////////////////////////////////////////
 
   newShot()
   var currentPlayer = whoseTurn();
@@ -123,48 +41,43 @@ $(document).ready(function() {
         setTimeout(newShot, 60)
         $("#" + currentPlayer.attempts[currentPlayer.attemptNum]).addClass('missed');
         currentPlayer.attemptNum++
-        turn++
-
-      });
+          turn++
+      })
     })
   }
 
   $('#btn2').click(function() {
     ($kick).stop()
     var result = Math.round(($kick.height() / $timer.height()) * 100)
-    console.log('into btn2 fncn')
-
     if (result < 95) {
       alert(currentPlayer.score + " you missed it!  " + result)
       setTimeout(newShot, 60);
       $("#" + currentPlayer.attempts[currentPlayer.attemptNum]).addClass('missed');
       currentPlayer.attemptNum++
-      turn++
-
+        turn++
     } else {
       currentPlayer.score += 1
       $("#" + currentPlayer.attempts[currentPlayer.attemptNum]).addClass('goal');
       currentPlayer.attemptNum++
-      turn++
-      console.log(whoseTurn())
+        turn++
+        console.log(whoseTurn())
       alert(currentPlayer.score + " It's a goal!  ")
       setTimeout(newShot, 600)
-      // goal ()
-      console.log('hi3')
-      //
-      // function goal(){
-      //   for(var i = 0; i<currentPlayer.attempts; i++)
-      //   console.log('goal')
-      //   if(currentPlayer.score !== 0){
-      //     $(this).addClass('goal');
-      //   }
-      //   else {
-      //     console.log('missed')
-      //
-      //     $(this).addClass('missed')
-          // currentPlayer.addClass('missed');
-        }
-      })
+    }
+    if (player2.attemptNum === 5) {
+      if (player1.score === player2.score) {
+        stop()
+        console.log("it's a tie")
+      } else if (player1.score > player2.score) {
+        stop()
+        console.log("Player 1 win")
+      } else {
+        stop()
+        console.log('player 2 wins')
+      }
+    }
+
+  })
 
   function newShot() {
     currentPlayer = whoseTurn();
@@ -175,6 +88,33 @@ $(document).ready(function() {
     kicked = false
   }
 
+  //AFTER WE HAVE A WINNER WE NEED TO STOP THE EVENT LISTENER ON
+  // KICKING AND OFFER THE OPTION TO RESTART THE GAME
+  function stop(){
+    $('#btn').unbind('click')
+    $('#btn2').unbind('click')
+  }
+
+  //NEED TO RESTART THE GAME AFTER A WINNER IS DEFINED
+  //  - SET PLAYER.SCORE, PLAYER.ATTEMPS AND TURN TO 0
+  function restart(){
+    player1.score = 0
+    player2.score = 0
+    player1.attemptNum = 0
+    player2.attemptNum = 0
+    $('.attempt').removeClass('.missed','goal')
+    turn = 0
+
+  }
+
+
+  // ON A TIE WE NEED TO START A SECOND GAME MODE TO ONE GOAL FOR EACH
+  // PLAYER
+  // CLEAR SCOREBOARD ON SCREEN KEEPING RECORD OF GENERAL SCORE
+  // ONCE A WINNER IS DEFINED DISPLAY A MESSAGE WITH OVERALL SCORE
+  function onTie(){
+
+  }
   function whoseTurn() {
     return (turn % 2 ? player2 : player1)
   }
